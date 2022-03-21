bcd85e1cf4c0caa339c33574eeeebe5db3325302
Serov Sergey <Moonlight234@yandex.ru>
2022-03-21 17:49:28 +0200

    init worker model

--- back/epam/models.py
+++ back/epam/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
-
 class Country(models.Model):
     country_code = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date added')
+    pub_date = models.DateTimeField('date added', auto_now_add=True)
 
     def __str__(self):
         return self.country_code
@@ -12,12 +11,12 @@ class Country(models.Model):
 class Stat(models.Model):
     country_code = models.ForeignKey(Country, on_delete=models.CASCADE)
     date_value = models.DateField()
-    confirmed = models.PositiveIntegerField()
-    deaths = models.PositiveIntegerField()
+    confirmed = models.PositiveIntegerField(null=True, blank=True)
+    deaths = models.PositiveIntegerField(null=True, blank=True)
     stringency_actual = models.DecimalField(null=True, blank=True, max_digits=7, decimal_places=2)
-    stringency = models.DecimalField(max_digits=7, decimal_places=2)
+    stringency = models.DecimalField(null=True, blank=True, max_digits=7, decimal_places=2)
     stringency_legacy = models.DecimalField(null=True, blank=True, max_digits=7, decimal_places=2)
-    stringency_legacy_disp = models.DecimalField(max_digits=7, decimal_places=2)
+    stringency_legacy_disp = models.DecimalField(null=True, blank=True, max_digits=7, decimal_places=2)
 
     def __str__(self):
         return self.country_code.country_code + "    " + str(self.date_value)
