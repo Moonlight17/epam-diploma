71a36a4f5c043b1f11a3d7b3f085939733c4e675
Serov Sergey <Moonlight234@yandex.ru>
2022-03-26 13:39:00 +0300

    added return data of countries

--- back/epam/views.py
+++ back/epam/views.py
@@ -1,7 +1,8 @@
-import requests, datetime
+import requests, datetime, json
 from django.http import HttpResponse, JsonResponse
 from epam.models import *
 from epam.serializers import CountrySerializer, StatSerializer
+from rest_framework.response import Response
 from rest_framework import generics
 
 def index(request):
@@ -13,14 +14,30 @@ class Country_list(generics.ListAPIView):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
 
-class Country_list(generics.ListAPIView):
-    queryset = Country.objects.all()
-    serializer_class = CountrySerializer
+    # def get(self, request):
+    #     queryset = Country.objects.all()
+    #     return Response()
 
+# class Country_list(generics.ListAPIView):
+#     queryset = Country.objects.all()
+#     serializer_class = CountrySerializer
+#
 
 class Stat_list(generics.ListAPIView):
     queryset = Stat.objects.all()
     serializer_class = StatSerializer
+
+    def post(self, request):
+        data = json.loads(request.body)
+        need = data['data']
+        query = {}
+        inner_qs = Country.objects.filter(country_code__in=need)
+        for country in inner_qs:
+            data = Stat.objects.filter(country_code__exact=country)
+            serializer = StatSerializer(data, many=True)
+
+            query[country.country_code] = serializer.data
+        return Response(query)
 
 def data_from_api(request):
     response = requests.get('https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/date-range/2022-03-10/2022-04-12')
