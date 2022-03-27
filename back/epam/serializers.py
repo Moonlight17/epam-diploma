6a6f15e183c5af95ec0ce9ba63f08c0f5a88965b
Serov Sergey <Moonlight234@yandex.ru>
2022-03-27 14:04:27 +0300

    added updated serializers.py (changed fields)

--- back/epam/serializers.py
+++ back/epam/serializers.py
@@ -3,13 +3,17 @@ from rest_framework import serializers
 
 
 class CountrySerializer(serializers.ModelSerializer):
+    text = serializers.CharField(source='country_code')
+
     class Meta:
         model = Country
-        fields = '__all__'
+        fields = ['id', 'text', 'pub_date', 'count_data']
 
 
 class StatSerializer(serializers.ModelSerializer):
     country_name = serializers.CharField(source='country_code')
+
     class Meta:
         model = Stat
-        fields = ['date_value', 'confirmed', 'deaths', 'stringency_actual', 'stringency', 'stringency_legacy', 'stringency_legacy_disp', 'country_name', 'country_code']
+        fields = ['id', 'date_value', 'confirmed', 'deaths', 'stringency_actual', 'stringency', 'stringency_legacy', 'stringency_legacy_disp', 'country_name', 'country_code']
+        # fields = '__all__'
