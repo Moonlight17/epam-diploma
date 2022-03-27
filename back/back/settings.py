1e88e139e833e5dfe3f0bbb42ca9ba9f69c993c5
Serov Sergey <Moonlight234@yandex.ru>
2022-03-27 14:05:10 +0300

    added datetime_format & date_format for rest_framework

--- back/back/settings.py
+++ back/back/settings.py
@@ -141,3 +141,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DATETIME_FORMAT': "%d/%m/%Y %H:%M:%S",
+    'DATE_FORMAT': "%d/%m/%Y",
+}
