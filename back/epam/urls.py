83d01c891837be5cd10d370e809ac18a7c5175c4
Serov Sergey <Moonlight234@yandex.ru>
2022-03-31 16:56:35 +0300

    added urls for static file

--- back/epam/urls.py
+++ back/epam/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -8,4 +10,4 @@ urlpatterns = [
     # path('<int:pk>/', views.DetailStat.as_view(), name='detail'),
     path('country/', views.Country_list.as_view(), name='country'),
     path('stat/', views.Stat_list.as_view(), name='stat'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
